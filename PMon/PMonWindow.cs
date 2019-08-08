2227b24222e2edb81cadb2ab5b2517b34f63e8c4
mkaraki <backtrack1994+github@gmail.com>
2019-08-09 03:22:03 +0900

    Keeptime update

--- PMon/PMonWindow.cs
+++ PMon/PMonWindow.cs
@@ -8,41 +8,39 @@ namespace PMon
 {
     public partial class PMonWindow : Form
     {
-        private ulong CurrentTime = 0;
         private int KeepTime = 30;
 
-        private Dictionary<string, Series> Disk;
+        private Dictionary<string, int> Disk;
 
-        private Dictionary<string, Series> TXNIC;
-        private Dictionary<string, Series> RXNIC;
+        private Dictionary<string, int> TXNIC;
+        private Dictionary<string, int> RXNIC;
+
+        private Dictionary<int, List<double>> Charts = new Dictionary<int, List<double>>();
+        private Dictionary<int, Series> ChartIdPairs = new Dictionary<int, Series>();
 
         public PMonWindow()
         {
             InitializeComponent();
-        }
 
-        private double CurrentTimeData()
-        {
-            return CurrentTime;
+            foreach (var series in chart_main.Series)
+            {
+                RegisterChart(series);
+            }
         }
 
         private void Timer_update_Tick(object sender, EventArgs e)
         {
-            CurrentTime++;
-
-            double tdata = CurrentTimeData();
-
             #region CPU
 
-            chart_main.Series[0].Points.AddXY(tdata, PC_CPU.CPUUtilization());
+            Charts[0].Add(PC_CPU.CPUUtilization());
 
-            chart_main.Series[1].Points.AddXY(tdata, PC_CPU.CPUKernelTime());
+            Charts[1].Add(PC_CPU.CPUKernelTime());
 
             #endregion CPU
 
             #region RAM
 
-            chart_main.Series[2].Points.AddXY(tdata, PC_RAM.UsedRAM());
+            Charts[2].Add(PC_RAM.UsedRAM());
 
             #endregion RAM
 
@@ -50,7 +48,7 @@ namespace PMon
 
             foreach (string dletter in PC_DISK.Drives)
             {
-                Disk[dletter].Points.AddXY(tdata, PC_DISK.GetDiskPerc(dletter));
+                Charts[Disk[dletter]].Add(PC_DISK.GetDiskPerc(dletter));
             }
 
             #endregion Drive
@@ -59,18 +57,30 @@ namespace PMon
 
             foreach (string netname in PC_NIC.NICNames)
             {
-                TXNIC[netname].Points.AddXY(tdata, PC_NIC.GetTX(netname));
-                RXNIC[netname].Points.AddXY(tdata, PC_NIC.GetRX(netname));
+                Charts[TXNIC[netname]].Add(PC_NIC.GetTX(netname));
+                Charts[RXNIC[netname]].Add(PC_NIC.GetRX(netname));
             }
 
             #endregion Network
 
-            if (tdata >= KeepTime)
-            {
-                for (var i = 0; i < chart_main.ChartAreas.Count; i++)
+            UpdateChartView();
+        }
+
+        private void UpdateChartView()
+        {
+            if (Charts[0].Count >= KeepTime)
+                foreach (var kvp in Charts)
                 {
-                    chart_main.ChartAreas[i].AxisX.ScaleView.Zoom(tdata - KeepTime, tdata);
+                    kvp.Value.RemoveRange(0, Charts[kvp.Key].Count - KeepTime);
                 }
+
+            Console.WriteLine(Charts[0].Count);
+
+            foreach (var p in ChartIdPairs)
+            {
+                p.Value.Points.Clear();
+                for (int i = 0; i < Charts[p.Key].Count; i++)
+                    p.Value.Points.AddXY(i, Charts[p.Key][i]);
             }
         }
 
@@ -79,7 +89,7 @@ namespace PMon
             chart_main.ChartAreas[1].Axes[1].Maximum = PC_RAM.FullRam;
 
             await Task.Run(() => PC_DISK.Init());
-            Disk = new Dictionary<string, Series>();
+            Disk = new Dictionary<string, int>();
             foreach (string dletter in PC_DISK.Drives)
             {
                 Series series = new Series
@@ -89,13 +99,13 @@ namespace PMon
                     ChartType = SeriesChartType.Line
                 };
 
-                Disk.Add(dletter, series);
+                Disk.Add(dletter, RegisterChart(series));
                 chart_main.Series.Add(series);
             }
 
             await Task.Run(() => PC_NIC.Init());
-            TXNIC = new Dictionary<string, Series>();
-            RXNIC = new Dictionary<string, Series>();
+            TXNIC = new Dictionary<string, int>();
+            RXNIC = new Dictionary<string, int>();
             foreach (string netn in PC_NIC.NICNames)
             {
                 Series txs = new Series
@@ -105,7 +115,7 @@ namespace PMon
                     ChartType = SeriesChartType.Line
                 };
 
-                TXNIC.Add(netn, txs);
+                TXNIC.Add(netn, RegisterChart(txs));
 
                 Series rxs = new Series
                 {
@@ -114,7 +124,7 @@ namespace PMon
                     ChartType = SeriesChartType.Line
                 };
 
-                RXNIC.Add(netn, rxs);
+                RXNIC.Add(netn, RegisterChart(rxs));
 
                 chart_main.Series.Add(txs);
                 chart_main.Series.Add(rxs);
@@ -144,7 +154,17 @@ namespace PMon
                 return;
 
             flowlp_cmain.Visible = !flowlp_cmain.Visible;
-            FormBorderStyle = flowlp_cmain.Visible? FormBorderStyle.FixedSingle : FormBorderStyle.None;
+            FormBorderStyle = flowlp_cmain.Visible ? FormBorderStyle.FixedSingle : FormBorderStyle.None;
+        }
+
+        private int RegisterChart(Series series)
+        {
+            int newid = Charts.Count;
+
+            Charts.Add(newid, new List<double>());
+            ChartIdPairs.Add(newid, series);
+
+            return newid;
         }
     }
 }
